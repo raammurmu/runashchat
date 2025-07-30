d0d4a39ebd1b6373647797bacaf278176ec5beb3
Ram Murmu <runashInc@outlook.com>
2025-07-30 23:15:06 +0530

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,12 +1,10 @@
 export const ProjectOverview = () => {
   return (
-    <div className="flex flex-col items-center justify-end text-center mb-0 px-4">
-      <h1 className="text-3xl font-semibold mb-4 md:text-4xl font-bold mb-0 bg-gradient-to-r from-white to-white/80 bg-clip-text text-transparent drop-shadow-lg">What can I help with?</h1>
-      
-      {/* <p className="text-lg text-white/90 max-w-2xl mx-auto drop-shadow-md">
-          Streamline your business with AI-powered automation, live streaming tools, and intelligent product management.
-        </p> */}
-    
-   </div>
+        <div className="flex flex-col items-center justify-end">
+         <div className="flex flex-col items-center justify-end text-cemter mb-12 px-4">
+            <h1 className="text-3xl font-semibold mb-4">RunAshChat</h1>
+           </div>
+          </div>
+   
   );
 }
