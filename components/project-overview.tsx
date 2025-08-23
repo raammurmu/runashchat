f3ce2ee68991838fa8554dc7f8549fd3bfbf6378
Ram Murmu <runashInc@outlook.com>
2025-08-23 23:31:29 +0530

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,8 +1,8 @@
 export const ProjectOverview = () => {
   return (
         <div className="flex flex-col items-center justify-end">
-         <div className="flex flex-col items-center justify-end text-cemter mb-12 px-4">
-            <h1 className="text-2xl font-semibold mb-1">RunAshChat Agent</h1>
+         <div className="flex flex-col items-center justify-end text-cemter mb-6 px-4">
+            <h1 className="text-3xl font-semibold">Welcome to RunAshChat</h1>
            </div>
           </div>
    
